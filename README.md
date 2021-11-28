e5fb5c6a2a4a649786a0f48730a696e9f37f97e8
benja971 <niddam.benjamin@orange.fr>
2021-11-28 00:55:08 -0400

    Experiences

--- README.md
+++ README.md
@@ -7,10 +7,11 @@
     -   Les variables
     -   Les agents
         -   Leurs interactions
--   Exprériences et résultats
+-   Exprériences, résultats et limites
+
 -   Conclusion
 
-Le but de cette étude est de mettre en place une simulation de diffusion d'une maladie dans une population d'agents mobiles et d'étudier les différents moyens d'endiguer la maladie. Dans nos expériences, nous ferons varier différents paramètres de la simulation, comme le nombre d'agents, le temps, le nombre d'agents contaminés au temps zéro, etc. Nous présenterons ici les différentes méthodes d'endiguage de la maladie ainsi que les résultats obtenus.
+<div style="page-break-after: always;"></div>
 
 ## Présentation du modèle
 
@@ -31,3 +32,23 @@ Nos agents sont représentés par des cercles de coordonnées (x, y) et de rayon
 ### Définition des intéractions
 
 Il n'existe qu'un seul type d'interaction entre nos agents. C'est l'interaction de contagion. Lorsqu'un agent est contaminé, il peut infecter d'autres agents mais ne peut plus être contaminé. Pour qu'un agent sain se fasse contaminé, il doit respecter certaines conditions. Il doit être un rayon de contamination (ir) défini. Il doit être sain, ne pas être immunisé et ne pas être vacciné.
+
+## Exprériences et résultats
+
+Dans toutes nos simulations nous utilisons un nombre d'agents fixé à 1000, un nombre d'agents contaminés au temps zéro fixé à 1 et un degré de contamination à 3.5%.
+
+Avant de commencer à endiguer la maladie, nous avons simuler l'évolution de celle-ci sans mesures de restrictions. Voici le résultat obtenu:
+
+![Natural immunity only](./Courbes/1000_agents_1_contaminé_5_ans/natural_immunity_only.png)
+
+On observe des vagues de guérisons et de contaminations qui s'étendent jusqu'à la fin de la simulation. Ces vagues sont dues au fait que nos agents obtinnent une immunité naturelle temporaire après chaque guérison.
+
+### Exprériences et résultats
+
+Nous avons mit en place plusieurs expériences pour tenter de ralentir ou d'éradiquer la maladie. Pour chaque expérience, nous avons conserver les mêmes paramètres pour la simulation et répéter les simulations 5 fois pour réduire les incertitudes dues à l'aléatoire.
+
+Comme première expérience nous avons décidé de confiner la population. Pour ce faire nous divisons les valeurs de vx et vy par deux, puis par 3 et enfin par 5. Ce qui nous ramène à trois tests que nous appellons "confinement leger", "confinement moyen" et "confinement strict". Ceci entraine une réduction des interactions entre les agents. On cherche à savoir quel niveau de confinement permettrait d'éradiquer la maladie et sur quelle durée il faudrai le mettre en place. Nous avons donc trois résultats:
+
+Dans un deuxieme temps, nous avons décidé de mettre en place un respect des gestes barrières suivant. Nous avons mit en place trois types de gestes barrières: "Basics barrier gestures", "Mediums barrier gestures" et "Heavys barrier gestures". Ces trois types de gestes barrières ont une influence sur les interactions entre les agents. Nous avons donc trois résultats:
+
+-   La mise en place d'un vaccin
--- model.py
+++ model.py
@@ -5,179 +5,189 @@ import numpy as np
 import matplotlib.pyplot as plt
 from time import time
 
-def main(name, conf, gestesB, nbr_vacc):
-	# Initialisation
 
-	w, h = 5000, 5000
-	container = Container(w, h)
+def main(name, conf, gestesB, nbr_vacc, history, t):
+    # Initialisation
 
-	confinement = conf
-	gestesBarrieres = gestesB
-	container.agents = [Agent(i, randint(10, w-10), randint(10, h-10), randint(-20, 20) // confinement, randint(-20, 20) // confinement, 0.035/gestesBarrieres) for i in range(1000)]
+    w, h = 5000, 5000
+    container = Container(w, h)
 
-	patient0 = choice(container.agents)
+    confinement = conf
+    gestesBarrieres = gestesB
+    container.agents = [Agent(i, randint(10, w-10), randint(10, h-10), randint(-20, 20) //
+                              confinement, randint(-20, 20) // confinement, 0.035/gestesBarrieres) for i in range(1000)]
 
-	container.healthys = [*container.agents]
+    patient0 = choice(container.agents)
 
-	patient0.get_infected(0, container)
-	i = 0
+    container.healthys = [*container.agents]
 
-	# t = int(input("Nombre de tours : "))
-	t = 1825
+    patient0.get_infected(0, container)
+    i = 0
 
-	infected_history = [None] * t
-	healthy_history = [None] * t
-	vaccinated_history = [None] * t
-	immune_history = [None] * t
+    while i < t:
+        nbr_v = nbr_vacc
+        tours = 0
+        # print("{}/{}".format(i, t), end="    \r")
 
+        history["infected_history"][i] = len(container.infecteds)/5
+        history["healthy_history"][i] = len(container.healthys)/5
+        history["vaccinated_history"][i] = len(container.vaccinateds)/5
+        history["immune_history"][i] = len(container.immunes)/5
 
-	while i < t:
-		nbr_v = nbr_vacc
-		tours = 0
-		# print("{}/{}".format(i, t), end="    \r")
+        for agent in container.agents:
+            tours += 1
+            agent.move(container)
 
-		infected_history[i] = len(container.infecteds)/5
-		healthy_history[i] = len(container.healthys)/5
-		vaccinated_history[i] = len(container.vaccinateds)/5
-		immune_history[i] = len(container.immunes)/5
+            if agent.infected:
+                tours += agent.infect_neighbours(container, i)
+                agent.check_for_getting_healthy(i, container)
 
-		for agent in container.agents:	
-			tours += 1
-			agent.move(container)
+            if agent.immune:
+                agent.remove_immunity(container, i)
 
-			if agent.infected:
-				tours += agent.infect_neighbours(container, i)
-				agent.check_for_getting_healthy(i, container)
+            if agent.vaccinate:
+                agent.remove_vaccination(container, i)
 
-			if agent.immune:
-				agent.remove_immunity(container, i)
+        l = list(
+            filter(lambda a: not a.vaccinate and not a.dejaetevaccine, container.healthys))
 
-			if agent.vaccinate:
-				agent.remove_vaccination(container, i)
-		
-		
-		l = list(filter(lambda a: not a.vaccinate and not a.dejaetevaccine , container.healthys))
+        if len(l) < nbr_v:
+            nbr_v = len(l)
 
-		if len(l) < nbr_v:
-			nbr_v = len(l)
+        container.vaccine_people(sample(l, nbr_v), i)
 
-		container.vaccine_people(sample(l, nbr_v), i)
+        i += 1
 
-		i += 1
+        # print("time: {}, infecteds: {}, healthys: {}, len(l): {} nbr_v/j: {}, vaccinates: {}, tours: {}".format(i, len(container.infecteds), len(container.healthys), len(l), nbr_v, len(container.vaccinateds),tours), end="                                              \r")
 
-		# print("time: {}, infecteds: {}, healthys: {}, len(l): {} nbr_v/j: {}, vaccinates: {}, tours: {}".format(i, len(container.infecteds), len(container.healthys), len(l), nbr_v, len(container.vaccinateds),tours), end="                                              \r")
+    # print(sum(infected_history))
 
-	sainsmoinsimmunes = []
-
-
-	for i in range(len(healthy_history)):
-		sainsmoinsimmunes.append(healthy_history[i] - immune_history[i])
-
-	# print(sum(infected_history))
-
-	temps = np.arange(0, t, 1)
-	plt.plot(temps, infected_history, label="infected", color="red", linewidth=2, markerfacecolor="red", markeredgecolor="red")
-	plt.plot(temps, vaccinated_history, label="vaccinated", color="green", linewidth=2, markerfacecolor="green", markeredgecolor="green")
-	plt.plot(temps, sainsmoinsimmunes, label="healthy - immunes", color="blue", linewidth=2, markerfacecolor="blue", markeredgecolor="blue")
-	plt.plot(temps, immune_history, label="immune", color="yellow", linewidth=2, markerfacecolor="yellow", markeredgecolor="yellow")
-	plt.title(name)
-	plt.legend()
-	plt.xlabel("Time")
-	plt.ylabel("Number of agents")
-	# plt.show()
-	plt.savefig("./Courbes/1000_agents_1_contaminé_5_ans/{}.png".format(name))
-	plt.close()
 
 if __name__ == "__main__":
-	
-	names = ["Natural_immunity_only", "Light_confinement", "Partial_confinement", "Strict_confinement", "Basics_barrier_gestures", "Mediums_barrier_gestures", "Heavys_barrier_gestures", "Vaccin01", "Vaccin03", "Vaccin04", "Vaccin05"]
-	# names = ["Vaccin01_repeat", "Vaccin03_repeat", "Vaccin04_repeat", "Vaccin05_repeat"]
-	# names = ["Vaccin01", "Vaccin03", "Vaccin04", "Vaccin05"]
-	# names = ["Light_confinement", "Partial_confinement", "Strict_confinement"]
 
-	confinement = 1
-	gestesBarrieres = 1
-	nbr_vacc = 0
+    names = ["Natural_immunity_only", "Light_confinement", "Partial_confinement", "Strict_confinement", "Basics_barrier_gestures",
+             "Mediums_barrier_gestures", "Heavys_barrier_gestures", "Vaccin01", "Vaccin03", "Vaccin04", "Vaccin05"]
+    # names = ["Vaccin01_repeat", "Vaccin03_repeat", "Vaccin04_repeat", "Vaccin05_repeat"]
+    # names = ["Vaccin01", "Vaccin03", "Vaccin04", "Vaccin05"]
+    # names = ["Light_confinement", "Partial_confinement", "Strict_confinement"]
 
-	# start time count
-	start = time()
+    confinement = 1
+    gestesBarrieres = 1
+    nbr_vacc = 0
 
-	for name in names:
-		print(name)
+   # t = int(input("Nombre de tours : "))
+    t = 1825
 
-		if name == "Light_confinement":
-			confinement = 2
-			gestesBarrieres = 1
-			nbr_vacc = 0
+    # start time count
+    start = time()
 
-		elif name == "Partial_confinement":
-			confinement = 3
-			gestesBarrieres = 1
-			nbr_vacc = 0
+    for name in names:
+        print(name)
 
-		elif name == "Strict_confinement":
-			confinement = 5
-			gestesBarrieres = 1
-			nbr_vacc = 0
+        history = {
+            "infected_history": [None] * t,
+            "healthy_history": [None] * t,
+            "vaccinated_history": [None] * t,
+            "immune_history": [None] * t,
+            "sainsmoinsimmune": [None] * t,
 
-		elif name == "Basics_barrier_gestures":
-			confinement = 1
-			gestesBarrieres = 2
-			nbr_vacc = 0
+        }
 
-		elif name == "Mediums_barrier_gestures":
-			confinement = 1
-			gestesBarrieres = 3
-			nbr_vacc = 0
-		
-		elif name == "Heavys_barrier_gestures":
-			confinement = 1
-			gestesBarrieres = 5
-			nbr_vacc = 0
-		
-		elif name == "Vaccin01":
-			confinement = 1
-			gestesBarrieres = 1
-			nbr_vacc = 1
-		
-		elif name == "Vaccin03":
-			confinement = 1
-			gestesBarrieres = 1
-			nbr_vacc = 3
+        if name == "Light_confinement":
+            confinement = 2
+            gestesBarrieres = 1
+            nbr_vacc = 0
 
-		elif name == "Vaccin04":
-			confinement = 1
-			gestesBarrieres = 1
-			nbr_vacc = 4
+        elif name == "Partial_confinement":
+            confinement = 3
+            gestesBarrieres = 1
+            nbr_vacc = 0
 
-		elif name == "Vaccin05":
-			confinement = 1
-			gestesBarrieres = 1
-			nbr_vacc = 5
+        elif name == "Strict_confinement":
+            confinement = 5
+            gestesBarrieres = 1
+            nbr_vacc = 0
 
-		elif name == "Vaccin01_repeat":
-			confinement = 1
-			gestesBarrieres = 1
-			nbr_vacc = 1
-		
-		elif name == "Vaccin03_repeat":
-			confinement = 1
-			gestesBarrieres = 1
-			nbr_vacc = 3
-		
-		elif name == "Vaccin04_repeat":
-			confinement = 1
-			gestesBarrieres = 1
-			nbr_vacc = 4
-		
-		elif name == "Vaccin05_repeat":
-			confinement = 1
-			gestesBarrieres = 1
-			nbr_vacc = 5
+        elif name == "Basics_barrier_gestures":
+            confinement = 1
+            gestesBarrieres = 2
+            nbr_vacc = 0
 
-		for j in range(5):
-			main(name, confinement, gestesBarrieres, nbr_vacc)
+        elif name == "Mediums_barrier_gestures":
+            confinement = 1
+            gestesBarrieres = 3
+            nbr_vacc = 0
 
-	# end time count
-	end = time()
-	print("\nExecution time: {}".format(end - start))
+        elif name == "Heavys_barrier_gestures":
+            confinement = 1
+            gestesBarrieres = 5
+            nbr_vacc = 0
+
+        elif name == "Vaccin01":
+            confinement = 1
+            gestesBarrieres = 1
+            nbr_vacc = 1
+
+        elif name == "Vaccin03":
+            confinement = 1
+            gestesBarrieres = 1
+            nbr_vacc = 3
+
+        elif name == "Vaccin04":
+            confinement = 1
+            gestesBarrieres = 1
+            nbr_vacc = 4
+
+        elif name == "Vaccin05":
+            confinement = 1
+            gestesBarrieres = 1
+            nbr_vacc = 5
+
+        elif name == "Vaccin01_repeat":
+            confinement = 1
+            gestesBarrieres = 1
+            nbr_vacc = 1
+
+        elif name == "Vaccin03_repeat":
+            confinement = 1
+            gestesBarrieres = 1
+            nbr_vacc = 3
+
+        elif name == "Vaccin04_repeat":
+            confinement = 1
+            gestesBarrieres = 1
+            nbr_vacc = 4
+
+        elif name == "Vaccin05_repeat":
+            confinement = 1
+            gestesBarrieres = 1
+            nbr_vacc = 5
+
+        for j in range(5):
+            main(name, confinement, gestesBarrieres, nbr_vacc, history, t)
+
+        for i in range(len(history["healthy_history"])):
+            history["sainsmoinsimmune"][i] = history["healthy_history"][i] - \
+                history["immune_history"][i]
+
+        tmps = np.arange(0, t, 1)
+        plt.plot(tmps, history["infected_history"], label="infected", color="red",
+                 linewidth=2, markerfacecolor="red", markeredgecolor="red")
+        plt.plot(tmps, history["vaccinated_history"], label="vaccinated", color="green",
+                 linewidth=2, markerfacecolor="green", markeredgecolor="green")
+        plt.plot(tmps, history["sainsmoinsimmune"], label="healthy - immune", color="blue",
+                 linewidth=2, markerfacecolor="blue", markeredgecolor="blue")
+        plt.plot(tmps, history["immune_history"], label="immune", color="yellow",
+                 linewidth=2, markerfacecolor="yellow", markeredgecolor="yellow")
+
+        plt.title(name)
+        plt.legend()
+        plt.xlabel("Time")
+        plt.ylabel("Number of agents")
+        plt.savefig(
+            "./Courbes/1000_agents_1_contaminé_5_ans/{}.png".format(name))
+        plt.close()
+
+# end time count
+end = time()
+print("\nExecution time: {}".format(end - start))
